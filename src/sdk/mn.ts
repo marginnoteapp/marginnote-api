14283a1a2bc596642f828cd47460494c3b415a60
ourongxing <orongxing@gmail.com>
2022-11-02 21:30:07 +0800

    update api

--- src/sdk/mn.ts
+++ src/sdk/mn.ts
@@ -1,31 +1,23 @@
 import { OSType } from "../api"
 import type { UIColor } from "../api"
-
-function getVersion() {
-  return "3.7.19"
-}
+import { gte } from "semver"
 
 class MNAPP {
+  readonly app = Application.sharedInstance()
   get studyController() {
-    return Application.sharedInstance().studyController(
-      Application.sharedInstance().focusWindow
-    )
+    return this.app.studyController(this.app.focusWindow)
   }
   get notebookController() {
-    return Application.sharedInstance().studyController(
-      Application.sharedInstance().focusWindow
-    ).notebookController
+    return this.studyController.notebookController
   }
   get currentDocumentController() {
-    return Application.sharedInstance().studyController(
-      Application.sharedInstance().focusWindow
-    ).readerController.currentDocumentController
+    return this.studyController.readerController.currentDocumentController
   }
   get currentWindow() {
-    return Application.sharedInstance().focusWindow
+    return this.app.focusWindow
   }
   get currentThemeColor(): UIColor {
-    return this.themeColor[Application.sharedInstance().currentTheme!]
+    return this.themeColor[this.app.currentTheme!]
   }
   get currentAddon() {
     return {
@@ -34,23 +26,34 @@ class MNAPP {
     }
   }
   get currnetNotebookid() {
-    return Application.sharedInstance()?.studyController(
-      Application.sharedInstance().focusWindow
-    )?.notebookController?.notebookId
+    try {
+      return this.notebookController.notebookId
+    } catch {
+      return undefined
+    }
   }
   get currentDocmd5() {
-    // 只要在笔记本内，就算没有文档，也会存在一个 docmd5，只是长度变为了 32 位随机。尽量让这个值固定，8 个 0，避免生成更多的无效配置。
-    const { docMd5 } = Application.sharedInstance()?.studyController(
-      Application.sharedInstance().focusWindow
-    )?.readerController?.currentDocumentController
-    if (docMd5 && docMd5.length === 32) return "00000000"
-    else return docMd5
+    try {
+      // 只要在笔记本内，就算没有文档，也会存在一个 docmd5，只是长度变为了 32 位随机。尽量让这个值固定，8 个 0，避免生成更多的无效配置。
+      const { docMd5 } = this.currentDocumentController
+      if (docMd5 && docMd5.length === 32) return "00000000"
+      else return docMd5
+    } catch {
+      return undefined
+    }
   }
-  readonly isMac = Application.sharedInstance().osType == OSType.macOS
   readonly isZH = NSLocale.preferredLanguages()?.[0].startsWith("zh")
-  readonly app = Application.sharedInstance()
   readonly db = Database.sharedInstance()
-  readonly version = getVersion()
+  /**
+   * @version 4.0.2(97)
+   */
+  readonly version = this.app.appVersion ?? "3.7.21"
+  readonly buildNum = this.app.build ? Number(this.app.build) : 96
+  readonly isMNE = gte(this.version, "4.0.0")
+  readonly isMac = this.app.osType == OSType.macOS
+  readonly isMacMNE =
+    this.isMac && gte(this.version, "4.0.2") && this.buildNum >= 97
+  readonly isMacMN3 = this.isMac && !this.isMacMNE
   readonly themeColor = {
     Gray: UIColor.colorWithHexString("#414141"),
     Default: UIColor.colorWithHexString("#FFFFFF"),
